315fba4054308b69d73a862fdd6c1d8f90c6fb7f
takuseno <takuma.seno@gmail.com>
2018-05-13 14:56:04 +0900

    Add deepmind style parameters

--- atari_constants.py
+++ atari_constants.py
@@ -1,8 +1,16 @@
-STATE_SHAPE = [42, 42]
-STATE_WINDOW = 1
-CONVS = [[32, 3, 2], [32, 3, 2], [32, 3, 2], [32, 3, 2]]
-PADDING = 'SAME'
-FCS = []
+# DeepMind style
+STATE_SHAPE = [84, 84]
+STATE_WINDOW = 4
+CONVS = [[16, 8, 4], [32, 4, 2]]
+PADDING = 'VALID'
+FCS = [256]
+# OpenAI syle
+#STATE_SHAPE = [42, 42]
+#STATE_WINDOW = 1
+#CONVS = [[32, 3, 2], [32, 3, 2], [32, 3, 2], [32, 3, 2]]
+#PADDING = 'SAME'
+#FCS = []
+
 LSTM_UNIT = 256
 FINAL_STEP = 10 ** 8
 
@@ -10,8 +18,8 @@ LSTM = True
 POLICY_FACTOR = 1.0
 VALUE_FACTOR = 0.5
 ENTROPY_FACTOR = 0.01
-LR = 1e-4
-LR_DECAY = 'constant'
+LR = 7e-4
+LR_DECAY = 'linear'
 GRAD_CLIP = 40.0
 TIME_HORIZON = 20
 GAMMA = 0.99
--- train.py
+++ train.py
@@ -92,7 +92,7 @@ def main():
     lr = tf.Variable(constants.LR)
     decayed_lr = tf.placeholder(tf.float32)
     decay_lr_op = lr.assign(decayed_lr)
-    optimizer = tf.train.AdamOptimizer(lr)
+    optimizer = tf.train.RMSPropOptimizer(lr, decay=0.99, epsilon=0.1)
 
     master = make_agent(
         model, actions, optimizer, state_shape, phi, 'global', constants)
